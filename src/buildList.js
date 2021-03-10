8ca338ce715fd2365e61e948007593e07065b40a
gryincent <grygoriy@swipe.io>
2021-03-10 16:46:42 +1000

    add bsc mainnet token list

--- src/buildList.js
+++ src/buildList.js
@@ -1,11 +1,12 @@
 const { version } = require("../package.json");
 const mainnet = require("./tokens/mainnet.json");
+const bscmainnet = require("./tokens/bscmainnet.json");
 const ropsten = require("./tokens/ropsten.json");
 
 module.exports = function buildList() {
   const parsed = version.split(".");
   return {
-    name: "SwipeSwap Menu",
+    name: "SwipeSwap Token List",
     timestamp: new Date().toISOString(),
     version: {
       major: +parsed[0],
@@ -15,7 +16,7 @@ module.exports = function buildList() {
     tags: {},
     logoURI: "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x8CE9137d39326AD0cD6491fb5CC0CbA0e089b6A9/logo.png",
     keywords: ["swipeswap", "swipe", "default"],
-    tokens: [...mainnet, ...ropsten]
+    tokens: [...mainnet, ...ropsten, ...bscmainnet]
       // sort them by symbol for easy readability
       .sort((t1, t2) => {
         if (t1.chainId === t2.chainId) {
